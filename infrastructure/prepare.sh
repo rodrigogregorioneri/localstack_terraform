af41a4bf62628e9026efadef4e206ab19f7619c9
RODRIGO GREGORIO NERI <rodrigogregorioneri@hotmail.com>
2021-09-09 12:22:55 -0300

    subindo versao 3 - docker-compose custom

--- infrastructure/prepare.sh
+++ infrastructure/prepare.sh
@@ -3,6 +3,7 @@ empty=""
 for i in *.json; do aws --endpoint-url=http://localhost:4566 iam create-role --role-name "${i}" --assume-role-policy-document "file://${i}"; done
 cd ..
 cd ./lambdas
-for i in *.zip; do aws --endpoint-url=http://localhost:4566 lambda create-function --function-name "${i}" --zip-file "fileb://${i}" --handler index.handler --runtime nodejs12.x --role arn:aws:iam::000000000000:role/roleLambdaOlaMundo; done
+for i in *.zip; do aws --endpoint-url=http://localhost:4566 lambda create-function --function-name "${i/.zip/$empty}" --zip-file "fileb://${i}" --handler index.handler --runtime nodejs12.x --role arn:aws:iam::000000000000:role/roleLambdaOlaMundo; done
+
 for i in *.zip; do aws --endpoint-url=http://localhost:4566 logs create-log-group --log-group-name "/aws/lambda/${i/.zip/$empty}"; done
 rm -f {*.zip,.*.zip}
